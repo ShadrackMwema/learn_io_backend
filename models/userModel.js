0f431e09ff8f62954e1704942a33ad21f1015f7f
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-10-18 01:32:24 +0300

    success in authentication

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,6 +21,18 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please confirm your password']
     }
 });
+
+// Encrypt password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// Instance method to check if passwords match
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
